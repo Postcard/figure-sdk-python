c96cd42741a388cf5cf21c06352e3fc41c75a511
myname <jblazy93@gmai.com>
2020-04-02 16:23:16 +0200

    adding support to Py 3.8 to setup.py

--- setup.py
+++ setup.py
@@ -60,6 +60,8 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.8',
+
     ],
 
     # What does your project relate to?
